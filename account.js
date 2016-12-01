3e442be9415fb22c49d97e2d5c6d46d1a9f687c7
Alex Bazlinton <abazlinton@gmail.com>
2016-12-01 14:15:36 +0000

    balance test passes

---
+++ account.js
@@ -0,0 +1,6 @@
+var Account = function(params) {
+  this.balance = params.balance;
+  this.name = params.name;
+};
+
+module.exports = Account;
--- specs/account_spec.js
+++ specs/account_spec.js
@@ -2,5 +2,14 @@ var assert = require("assert");
 var Account = require("../account");
 
 describe("Account", function(){
-  it("does some shit");
+
+  var account;
+
+  beforeEach(function(){
+    account = new Account({balance: 100, name: "Alex"});
+  });
+
+  it("has a balance", function(){
+    assert.equal(100, account.balance);
+  });
 });
