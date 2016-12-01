055971b653c58cf9239125fdc9b8cfa55f4e21d4
Alex Bazlinton <abazlinton@gmail.com>
2016-12-01 14:31:28 +0000

    can hold accounts test passed

--- specs/bank_spec.js
+++ specs/bank_spec.js
@@ -7,12 +7,18 @@ describe("Bank", function(){
   var bank;
 
   beforeEach(function(){
-    var testAccounts = [];
+    var testAccounts = [{name: "Cyrus", balance: 1000, type: "legit"}];
     bank = new Bank({name: "Legit (honest!) Ltd.", accounts: testAccounts});
   });
 
   it("has a name", function(){
     assert.equal("Legit (honest!) Ltd.", bank.name);
   });
+
+  it("can hold accounts", function(){
+    assert.equal("Cyrus", bank.accounts[0].name);
+  });
+
+
 
 });
